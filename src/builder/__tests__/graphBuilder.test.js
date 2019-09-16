73b033af81e71fd0783f068fb7e36361b878c732
equine <matthew.the.equine@gmail.com>
2019-09-16 23:13:58 +0200

    addEdge builder mvp

--- src/builder/__tests__/graphBuilder.test.js
+++ src/builder/__tests__/graphBuilder.test.js
@@ -6,8 +6,9 @@ describe('graph builder', () => {
     // given
     const graph = {
       V: [],
-      E: [], 
-      phi: {}
+      E: [],
+      phi: {},
+      adj: {}
     }
     const build = builder(graph)
 
@@ -18,5 +19,40 @@ describe('graph builder', () => {
 
     // then
     expect(graph.V).members([1, 2])
+  })
+
+  const addEdge = [
+    { type: 'undirected', adj: { 1: [1], 2: [1] } },
+    { type: 'directed', adj: { 1: [1], 2: [] } }
+  ]
+
+  addEdge.forEach(({ type, adj }) => {
+    it('should add edge', () => {
+      // given
+      const graph = {
+        V: [],
+        E: [],
+        phi: {},
+        adj: {}
+      }
+      const build = builder(graph)
+
+      // when
+      build
+        .addVertex(1)
+        .addVertex(2)
+        .addEdge(1, 1, 2, type)
+
+
+      // then
+      expect(graph).eqls({
+        V: [1, 2],
+        E: [1],
+        phi: {
+          '1': { from: 1, to: 2, type }
+        },
+        adj
+      })
+    })
   })
 })
--- src/builder/graphBuilder.js
+++ src/builder/graphBuilder.js
@@ -1,10 +1,18 @@
-const builder = graph => {
-  return {
-    addVertex: vertex => {
-      graph.V.push(vertex)
-      return builder(graph)
-    }
+const builder = graph => ({
+  addVertex: vertex => {
+    graph.V.push(vertex)
+    graph.adj[vertex] = []
+
+    return builder(graph)
+  },
+  addEdge: (edge, from, to, type) => {
+    graph.E.push(edge)
+    graph.phi[edge] = { from, to, type }
+    graph.adj[from].push(edge)
+    type === 'undirected' && graph.adj[to].push(edge)
+
+    return builder(graph)
   }
-}
+})
 
 export default builder
